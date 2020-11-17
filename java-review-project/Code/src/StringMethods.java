2f0b6fec43697e8d1efbe0a44e1167e9755ab011
saimCybertek <saim@cybertekschool.com>
2020-11-17 15:36:24 -0600

    String tasks

--- java-review-project/Code/src/StringMethods.java
+++ java-review-project/Code/src/StringMethods.java
@@ -1,7 +1,13 @@
 public class StringMethods {
 
     public static void main(String[] args) {
-        System.out.println(freqOfChar("bananana", 'a'));
+        System.out.println(freqOfChar("bananana", 'z'));
+
+        System.out.println();
+
+        freqOfEachChar("aaapppple");
+
+
     }
 
     // freqency of chars
@@ -11,11 +17,9 @@ public class StringMethods {
     // ("java", 'a')  -> 2
 
     public static int freqOfChar (String word, char letter) {
-
         int count = 0;
 
         for(int i=0; i < word.length(); i++) {
-
             if(word.charAt(i)  == letter) {
                 count++;
             }
@@ -23,6 +27,21 @@ public class StringMethods {
         return count;
     }
 
+    // We want to print how many times each char appears
+    // apple
+        // a - 1
+        // p - 2
 
+    public static void freqOfEachChar (String str) {
+        String checked = "";
+        for(int i=0; i < str.length(); i++) {
+            if(!checked.contains("" + str.charAt(i))) {
+                int freq = freqOfChar(str, str.charAt(i));
+                System.out.println(str.charAt(i) + " - " + freq);
+                checked += str.charAt(i) + " ";
+            }
+        }
+
+    }
 
 }
